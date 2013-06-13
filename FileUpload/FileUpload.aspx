622168e8f1a33a8212861f76e9e7e6ea34a5c9ec
weibing <wbing520@gmail.com>
2013-06-13 16:21:49 -0600

    File Upload System
    File Upload System. for fun

---
+++ FileUpload/FileUpload.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/RoseMaster.master" 
+AutoEventWireup="true" CodeFile="FileUpload.aspx.cs" Inherits="FileUpload" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <div>
+    <h2 style="color:Green">Please upload your CV and resume</h2>
+    <asp:FileUpload ID="fileupload1" runat="server" />
+    <br />
+    <br />
+    <asp:Button ID="button1" Text="Upload" runat="server" Width="73px" 
+        onclick="button1_Click" />
+    <br />
+    <br />
+    <asp:Label ID="Label1" runat="server" Font-Bold="True" ForeColor="#000099"></asp:Label>
+     <br /> <br /> <br />
+    <object data = "kkkkk.pdf" type = "application/pdf" width = "600" height = "400" ></object >
+</div>
+</asp:Content>
+
---
+++ FileUpload/FileUpload.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+public partial class FileUpload : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void button1_Click(object sender, EventArgs e)
+    {
+        if (fileupload1.HasFile)
+        {
+            try
+            {
+                if (fileupload1.PostedFile.ContentType == "application/pdf")
+                {
+                    if (fileupload1.PostedFile.ContentLength < 10012000)
+                    {
+                        string filename = Path.GetFileName(fileupload1.FileName);
+                        fileupload1.SaveAs(Server.MapPath("~/") + filename);
+                        string path = Server.MapPath("~/") + filename;
+                        Label1.Text = "File uploaded successfully!" + path;
+                       
+                    }
+                    else
+                        Label1.Text = "File maximum size is 10000 Kb";
+                }
+                else
+                    Label1.Text = "Only pdf files are accepted!";
+            }
+            catch (Exception exc)
+            {
+                Label1.Text = "The file could not be uploaded. The following error occured: " + exc.Message;
+            }
+        }
+    }
+}
+    
